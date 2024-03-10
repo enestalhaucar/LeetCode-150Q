513222909281ceb751e81762176b0e748c9ac9ed
Enes Talha Uçar <talhauenes@gmail.com>
2024-03-10 23:30:39 +0300

    Question 7 is solved

---
+++ src/Q7_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+
+public class Q7_BestTimeToBuyAndSellStock {
+
+	public static void main(String[] args) {
+		int prices[] = {7,1,5,3,6,4};
+		
+		System.out.println(maxProfit(prices));
+	}
+	public static int maxProfit(int[] prices) {
+        int profit = 0; 
+        int good = Integer.MAX_VALUE;
+        for ( int i = 0; i < prices.length; i ++) {
+            if ( prices[i] < good) {
+                good = prices[i];
+            }
+            else if (prices[i] - good > profit) {
+                profit = prices[i] - good;
+            }
+
+        }
+        return profit;
+    }
+
+}
