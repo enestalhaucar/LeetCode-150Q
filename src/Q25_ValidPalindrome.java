85596084ffedf33fb98c1c94ae18d14629225781
Enes Talha Uçar <talhauenes@gmail.com>
2024-03-14 19:56:38 +0300

    Question 25 is solved

---
+++ src/Q25_ValidPalindrome.java
@@ -0,0 +1,24 @@
+
+public class Q25_ValidPalindrome {
+
+	public static void main(String[] args) {
+		String sentence = "A man, a plan, a canal: Panama";
+		System.out.println(isPalindrome(sentence));	
+	}
+	
+
+
+	public static boolean isPalindrome(String sentence) {
+        String str = sentence.replaceAll("[^A-Za-z]+", "").toLowerCase();
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        String ans = sb.toString();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ans.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
